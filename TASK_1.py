85b3af68b0af8b2ba682966468b4a8d2e3be9fba
Hamza69-crypto <149509291+Hamza69-crypto@users.noreply.github.com>
2025-07-02 15:53:35 +0530

    Add files via upload

---
+++ TASK_1.py
@@ -0,0 +1,11 @@
+a=int(input("ENTER THE FIRST NUMBER: "))
+b=int(input("ENTER THE SECOND NUMBER: "))
+c=a+b
+print("THE SUM OF",a,"AND",b,"IS",c)
+c=a*b
+print("THE PRODUCT OF",a,"AND",b,"IS",c) 
+c=a-b
+print("THE DIFFERENCE OF",a,"AND",b,"IS",c)
+c=a/b
+print("THE DIVISION OF",a,"AND",b,"IS",c)
+c=a%b
